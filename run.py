3bf49e9785385b55477b40977aae63389c60a464
Konrad Plawik <konrad.plawik@itechart-group.com>
2021-11-10 15:03:12 +0100

    Proper port.

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app.app import create_app
 if __name__ == '__main__':
     app = create_app()
     # app.run(debug=False)
-    app.run(debug=False, host='0.0.0.0', port='19580')
+    app.run(debug=False, host='0.0.0.0', port='5001')
