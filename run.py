3c4d791a5999680dddb1fef58cbf33a9c75964ac
Tomasz Janicki <tomoicki@gmail.com>
2021-11-12 15:35:10 +0100

    Update run.py

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     handler = RotatingFileHandler('console_output.log')
     app.logger.addHandler(handler)
     # app.run(debug=False)
-    app.run(debug=False, host='0.0.0.0', port='5000')
+    app.run(debug=False, host='0.0.0.0', port='5001')
